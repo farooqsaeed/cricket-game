2e5a5dbf0e56b3c7871d8125989ffb9e647e26de
Farooq Saeed <60470178+farooqsaeed@users.noreply.github.com>
2022-09-02 11:27:34 +0500

    modified

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -119,7 +119,7 @@ class TeamController extends Controller
 
     public function TeamWithPlayer()
     {
-       $players = Team::with('players')->get();
+       $players = Team::with('players')->select('id','name','players')->get();
 
        return json_encode([
         'success'=>$players
